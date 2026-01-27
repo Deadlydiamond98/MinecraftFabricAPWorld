4745ba6e71d1ccea6aaeddd2cc4adb9153cc4419
deadlydiamond98 <diamonderrick1@gmail.com>
2026-01-27 08:36:02 -0500

    - Reorganized Items to make it easier for mod support later

--- worlds/minecraft_fabric/__init__.py
+++ worlds/minecraft_fabric/__init__.py
@@ -1,13 +1,12 @@
-import math
-from typing import Mapping, Any, List
+from typing import Mapping, Any
 
-from BaseClasses import ItemClassification, Item, Location
-from Options import OptionError
+from BaseClasses import ItemClassification, Item
 from worlds.AutoWorld import World
-from worlds.minecraft_fabric.items import item_table, useful_index, traps_index, bl_progression_index
+from worlds.minecraft_fabric.item.items_helper import item_table
+from worlds.minecraft_fabric.item.items_init import create_local_fill_items, create_items
 from worlds.minecraft_fabric.location.minecraft_locations import location_table
 from worlds.minecraft_fabric.options import FMCOptions
-from worlds.minecraft_fabric.region.minecraft_regions import create_regions
+from worlds.minecraft_fabric.region.regions_init import create_regions
 
 
 class FabricMinecraftWorld(World):
@@ -17,7 +16,7 @@ class FabricMinecraftWorld(World):
     topology_present = True
 
     item_name_to_id = {
-        item.name: item.item_id for item in item_table
+        item.name: item.item_id for item in item_table.values()
     }
 
     location_name_to_id = location_table
@@ -28,6 +27,9 @@ class FabricMinecraftWorld(World):
         self.max_ruby_count = 0
         self.itemsanity_locations = []
         self.local_fill_amount = 0
+
+    def create_regions(self):
+        create_regions(self)
 
     def fill_slot_data(self) -> Mapping[str, Any]:
         # from Utils import visualize_regions
@@ -71,109 +73,11 @@ class FabricMinecraftWorld(World):
             "randomize_chests": self.options.randomize_chests.value
         }
 
-    def create_regions(self):
-        create_regions(self)
-
     def create_item(self, name: str) -> "Item":
         return Item(name, ItemClassification.progression, self.item_name_to_id[name], self.player)
 
     def create_items(self):
-        total_items = len(self.multiworld.get_unfilled_locations(self.player))
-        self.local_fill_amount = math.floor(len(self.itemsanity_locations) * self.options.itemsanity_local_fill * 0.01)
-        total_items -= self.local_fill_amount
-
-        # Progression Items ############################################################################################
-
-        if self.options.randomize_swim:
-            total_items = self.add_to_pool(1, total_items)
-        if self.options.randomize_sprint:
-            total_items = self.add_to_pool(2, total_items)
-        if self.options.randomize_jump:
-            total_items = self.add_to_pool(3, total_items)
-        if self.options.randomize_chests:
-            total_items = self.add_to_pool(4, total_items)
-
-        progressive_index = 5
-
-        # Progressive Tools
-        total_items = self.add_multiple_to_pool(progressive_index, 4, total_items)
-        # Progressive Weapons
-        total_items = self.add_multiple_to_pool(progressive_index + 1, 4, total_items)
-        # Progressive Smelting
-        total_items = self.add_multiple_to_pool(progressive_index + 2, 2, total_items)
-        # Progressive Armor
-        total_items = self.add_multiple_to_pool(progressive_index + 3, 5, total_items)
-        # Progressive Archery
-        total_items = self.add_multiple_to_pool(progressive_index + 4, 2, total_items)
-
-        # Single Check Progressive Items
-        for i in range(bl_progression_index + 1, useful_index + 1):
-            total_items = self.add_to_pool(i, total_items)
-
-        # Ruby Hunt Items
-        if self.options.goal_condition.value == 4:
-            self.max_ruby_count = min(self.options.total_rubies.value, total_items)
-            total_items = self.add_multiple_to_pool(0, self.max_ruby_count, total_items)
-
-        for item in self.fill_traps_and_junk(total_items):
-            self.multiworld.itempool.append(item)
-
+        create_items(self)
 
     def pre_fill(self) -> None:
-        location_map: List[Location] = [self.multiworld.get_location(loc, self.player) for loc in self.itemsanity_locations]
-        self.random.shuffle(location_map)
-        filler_size = self.local_fill_amount
-        filler_items = self.fill_traps_and_junk(filler_size)
-
-        while filler_size > 0:
-            if len(location_map) == 0:
-                raise OptionError("Another AP world is attempting to mess with Minecraft Fabric's prefill locations, please go politely inform them of this blunder posthaste!")
-
-            location = location_map.pop()
-            if not location.locked:
-                location.place_locked_item(filler_items[filler_size - 1])
-                filler_size -= 1
-
-    def add_multiple_to_pool(self, index: int, count: int, total_items: int):
-        for i in range(count):
-            total_items = self.add_to_pool(index, total_items)
-        return total_items
-
-    def create_item_from_table(self, index: int):
-        return Item(item_table[index].name, item_table[index].item_type, item_table[index].item_id, self.player)
-
-    def add_to_pool(self, index: int, total_items: int):
-        self.multiworld.itempool.append(self.create_item_from_table(index))
-        total_items -= 1
-        return total_items
-
-    def add_trap_weight(self, index, weight):
-        return [item_table[index]] * weight
-
-    def fill_traps_and_junk(self, total_items: int):
-        junk_items = []
-
-        # Trap Items ###################################################################################################
-        trap_weights = []
-        trap_weights += self.add_trap_weight(traps_index + 1, self.options.reverse_controls_trap_weight)
-        trap_weights += self.add_trap_weight(traps_index + 2, self.options.inverted_mouse_trap_weight)
-        trap_weights += self.add_trap_weight(traps_index + 3, self.options.ice_trap_weight)
-        trap_weights += self.add_trap_weight(traps_index + 4, self.options.random_effect_trap_weight)
-        trap_weights += self.add_trap_weight(traps_index + 5, self.options.stun_trap_weight)
-        trap_weights += self.add_trap_weight(traps_index + 6, self.options.tnt_trap_weight)
-        trap_weights += self.add_trap_weight(traps_index + 7, self.options.teleport_trap_weight)
-        trap_weights += self.add_trap_weight(traps_index + 8, self.options.bee_trap_weight)
-        trap_weights += self.add_trap_weight(traps_index + 9, self.options.literature_trap_weight)
-        trap_count = 0 if (len(trap_weights) == 0) else math.ceil(
-            total_items * (self.options.trap_fill_percentage.value / 100.0))
-
-        for i in range(trap_count):
-            trap_item = self.random.choice(trap_weights)
-            junk_items.append(Item(trap_item.name, trap_item.item_type, trap_item.item_id, self.player))
-            total_items -= 1
-
-        # Filler Items #####################################################################################################
-        while total_items > 0:
-            junk_items.append(self.create_item_from_table(self.random.randint(useful_index + 1, traps_index)))
-            total_items -= 1
-        return junk_items
+        create_local_fill_items(self)
