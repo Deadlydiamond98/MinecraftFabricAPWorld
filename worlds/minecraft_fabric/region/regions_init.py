882e1502177130b512b2305e762ec1900f6aeac7
deadlydiamond98 <diamonderrick1@gmail.com>
2026-01-30 02:06:05 -0500

    - FIXED ACCIDENTAL DELETION OF GOAL LOGIC

--- worlds/minecraft_fabric/region/regions_init.py
+++ worlds/minecraft_fabric/region/regions_init.py
@@ -11,6 +11,22 @@ from worlds.minecraft_fabric.logic.vanilla_logic import *
 if TYPE_CHECKING:
    from worlds.minecraft_fabric import FabricMinecraftWorld
 
+def get_goal_condition(world, state):
+    goal_id = world.options.goal_condition.value
+
+    # I wish Python had Switch Case Statements :,(
+    if goal_id == 0: # Ender Dragon
+        return canGoalEnderDragon(world, state)
+    elif goal_id == 1: # Wither
+        return canGoalWither(world, state)
+    elif goal_id == 2: # Both Bosses
+        return canBeatDragonAndWither(world, state)
+    elif goal_id == 4: # Ruby Hunt
+        return canCompleteRubyHunt(world, state)
+
+    # Since Advancements are Locations, just make game try to reach end of game
+    return canAccessVanillaEndGame(world, state)
+
 # Creates all Regions in the Randomizer!
 def create_regions(world: FabricMinecraftWorld):
     # Creates a Main Region for everything to branch from!
@@ -18,3 +34,6 @@ def create_regions(world: FabricMinecraftWorld):
     # Vanilla Regions
     create_vanilla_advancement_regions(world)
     create_vanilla_itemsanity_regions(world)
+
+    world.multiworld.completion_condition[world.player] = lambda state: get_goal_condition(world, state)
+
